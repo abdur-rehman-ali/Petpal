80654bf1fbbef822e3d95a00b648175bbbf7cf10
abdur-rehman-ali <alijoyia911@gmail.com>
2025-01-13 03:31:49 +0500

    Add styling to logout page

--- authentication/settings.py
+++ authentication/settings.py
@@ -142,3 +142,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 TAILWIND_APP_NAME = "theme"
 INTERNAL_IPS = ["127.0.0.1"]
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+LOGIN_REDIRECT_URL = "/"
