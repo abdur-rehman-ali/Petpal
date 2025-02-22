8ca46079aa3a376caec017900cb84bd555893efe
abdur-rehman-ali <alijoyia911@gmail.com>
2025-02-22 05:42:11 +0500

    Added pagination in pets listing view

--- petlisting/admin.py
+++ petlisting/admin.py
@@ -4,6 +4,6 @@ from .models import Pet
 
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
-    list_display = ("name", "pet_type", "breed", "price", "status")
+    list_display = ("name", "pet_type", "breed", "price", "status", "created_at")
     list_filter = ("pet_type", "status")
     search_fields = ("name", "breed")
--- petlisting/views.py
+++ petlisting/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Pet
 
 
 # Create your views here.
 def list_view(request):
-    pets = Pet.objects.all()
+    pets = Pet.objects.all().order_by("-created_at")
+    paginator = Paginator(pets, 10)
+    page_number = request.GET.get("page")
+    page_object = paginator.get_page(page_number)
     template_name = "petlisting/listing.html"
-    context = {"pets": pets}
+    context = {"pets": page_object}
     return render(request, template_name, context=context)
