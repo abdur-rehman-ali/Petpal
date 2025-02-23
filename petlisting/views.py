029c9329813eb72fd6933a0a07642407cc9eadf2
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2025-02-23 05:38:11 +0500

    Added filters in petlisting

--- petlisting/views.py
+++ petlisting/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Pet
+from .filters import PetFilter
 
 
 # Create your views here.
 def list_view(request):
-    pets = Pet.objects.all().order_by("-created_at")
-    paginator = Paginator(pets, 10)
+    # Qeury pets_list lists
+    pets_list = Pet.objects.all().order_by("-created_at")
+    # Apply filters
+    pet_filters = PetFilter(request.GET, queryset=pets_list)
+    pets_list = pet_filters.qs
+    # Apply pagination
+    paginator = Paginator(pets_list, 10)
     page_number = request.GET.get("page")
     page_object = paginator.get_page(page_number)
+    # Render template
     template_name = "petlisting/listing.html"
-    context = {"pets": page_object}
+    context = {"pets": page_object, "filters": pet_filters}
     return render(request, template_name, context=context)
