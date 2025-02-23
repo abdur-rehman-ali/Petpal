008f892676491f66ec4566f74d80c33fc7e289c5
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2025-02-24 03:11:02 +0500

    Create view of petlisting

--- petlisting/urls.py
+++ petlisting/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_view, detail_view
+from .views import list_view, detail_view, create_view
 
 urlpatterns = [
     path("pets/", list_view, name="petlisting__list_view"),
     path("pets/<int:pet_id>/", detail_view, name="petlisting__detail_view"),
+    path("pets/create/", create_view, name="petlisting__create_view"),
 ]
