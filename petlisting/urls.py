0862221ffc75798dba473ca71eaee2896f91214c
Abdur rehman ali <58654786+abdur-rehman-ali@users.noreply.github.com>
2025-02-24 03:30:38 +0500

    Edit view of petlisting

--- petlisting/urls.py
+++ petlisting/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_view, detail_view, create_view
+from .views import list_view, detail_view, create_view, edit_view
 
 urlpatterns = [
     path("pets/", list_view, name="petlisting__list_view"),
     path("pets/<int:pet_id>/", detail_view, name="petlisting__detail_view"),
     path("pets/create/", create_view, name="petlisting__create_view"),
+    path("pets/<int:pet_id>/edit", edit_view, name="petlisting__edit_view"),
 ]
