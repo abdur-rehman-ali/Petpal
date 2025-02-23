2dc89c40d4f2fdb9c7ac181964797b36786bafed
abdur-rehman-ali <alijoyia911@gmail.com>
2025-02-23 06:09:22 +0500

    Detail view of petlisting

--- petlisting/urls.py
+++ petlisting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_view
+from .views import list_view, detail_view
 
 urlpatterns = [
-    path("list/", list_view, name="petlisting__list_view"),
+    path("pets/", list_view, name="petlisting__list_view"),
+    path("pets/<int:pet_id>/", detail_view, name="petlisting__detail_view"),
 ]
