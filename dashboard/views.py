4b92b65f2fa341ff4b28c41bfdc590de61622723
abdur-rehman-ali <alijoyia911@gmail.com>
2025-03-08 23:53:50 +0500

    Refactored view of user profile creation

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,17 +8,12 @@ from django.contrib import messages
 @login_required
 def profile_view(request):
     instance, _ = Profile.objects.get_or_create(user=request.user)
+    form = ProfileUpdateForm(request.POST or None, instance=instance)
     if request.method == "POST":
-        form = ProfileUpdateForm(request.POST, instance=instance)
         if form.is_valid():
             form.save()
             messages.success(request, "Profile has been successfully updated!!!")
             return redirect("dashboard_profile")
-        else:
-            messages.error(request, "Failed to updated user profile!!!")
-            return redirect("dashboard_profile")
-    else:
-        form = ProfileUpdateForm(instance=instance)
     template_name = "dashboard/profile.html"
-    context = {"user": request.user, "form": form}
-    return render(request, template_name=template_name, context=context)
+    context = {"form": form}
+    return render(request, template_name, context)
