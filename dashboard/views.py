05bf059f2d9bc1759cba13191f0df1f5d92a9cdb
abdur-rehman-ali <alijoyia911@gmail.com>
2025-03-08 23:27:33 +0500

    Added view of user profile creation

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
+from .models import Profile
+from django.contrib import messages
 
 
 @login_required
 def profile_view(request):
+    instance, _ = Profile.objects.get_or_create(user=request.user)
+    if request.method == "POST":
+        form = ProfileUpdateForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile has been successfully updated!!!")
+            return redirect("dashboard_profile")
+        else:
+            messages.error(request, "Failed to updated user profile!!!")
+            return redirect("dashboard_profile")
+    else:
+        form = ProfileUpdateForm(instance=instance)
     template_name = "dashboard/profile.html"
-    context = {"user": request.user}
+    context = {"user": request.user, "form": form}
     return render(request, template_name=template_name, context=context)
